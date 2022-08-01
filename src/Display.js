45da8c517d1be98d1a534260c4f38af7c72dd6f2
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-08-02 07:30:08 +1000

    rectified the error

--- src/Display.js
+++ src/Display.js
@@ -3,5 +3,6 @@ import { useSelector } from "react-redux";
 
 export const Display = () => {
   const { counter } = useSelector((state) => state.counter);
+  console.log(counter);
   return <div>{counter}</div>;
 };
--- src/addBtn.js
+++ src/addBtn.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import incrementCounter from "./counterSlice.js";
+import { incrementCounter } from "./counterSlice.js";
 
 export const AddBtn = () => {
   const dispatch = useDispatch();
